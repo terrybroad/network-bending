50559b4e996ba67d425c7222fab068546227b2e1
Terence Broad <terry.m.broad@gmail.com>
2020-04-21 20:12:04 +0100

    Implement and train clustering models for any resolution

--- clustering_models.py
+++ clustering_models.py
@@ -1,30 +1,152 @@
 import torch
+import collections
 from torch import nn
 from torch.nn import functional as F
 from torch.autograd import Function
 
-class FeatureClassifier_L1(nn.Module):
-    def __init__(self):
+layer_depth_dict = {
+    1 : 0,
+    2 : 0,
+    3 : 1,
+    4 : 1,
+    5 : 2,
+    6 : 2,
+    7 : 3,
+    8 : 3,
+    9 : 4,
+    10 : 4,
+    11 : 5,
+    12 : 5,
+    13 : 6,
+    14 : 6,
+    15 : 7,
+    16 : 7
+}
+
+layer_class_dict = {
+    1 : 512,
+    2 : 512,
+    3 : 512,
+    4 : 512,
+    5 : 512,
+    6 : 512,
+    7 : 512,
+    8 : 512,
+    9 : 256,
+    10 : 256,
+    11 : 128,
+    12 : 128,
+    13 : 64,
+    14 : 64,
+    15 : 32,
+    16 : 32
+}
+
+#based on https://github.com/pytorch/vision/blob/master/torchvision/models/shufflenetv2.py
+def channel_shuffle(x, groups):
+    # type: (torch.Tensor, int) -> torch.Tensor
+    batchsize, num_channels, height, width = x.data.size()
+    channels_per_group = num_channels // groups
+
+    # reshape
+    x = x.view(batchsize, groups,
+               channels_per_group, height, width)
+
+    x = torch.transpose(x, 1, 2).contiguous()
+
+    # flatten
+    x = x.view(batchsize, -1, height, width)
+
+    return x
+
+
+class InvertedResidual(nn.Module):
+    def __init__(self, inp, oup, stride):
+        super(InvertedResidual, self).__init__()
+
+        if not (1 <= stride <= 3):
+            raise ValueError('illegal stride value')
+        self.stride = stride
+
+        branch_features = oup // 2
+        assert (self.stride != 1) or (inp == branch_features << 1)
+
+        if self.stride > 1:
+            self.branch1 = nn.Sequential(
+                self.depthwise_conv(inp, inp, kernel_size=3, stride=self.stride, padding=1),
+                nn.BatchNorm2d(inp),
+                nn.Conv2d(inp, branch_features, kernel_size=1, stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(branch_features),
+                nn.ReLU(inplace=True),
+            )
+        else:
+            self.branch1 = nn.Sequential()
+
+        self.branch2 = nn.Sequential(
+            nn.Conv2d(inp if (self.stride > 1) else branch_features,
+                      branch_features, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(branch_features),
+            nn.ReLU(inplace=True),
+            self.depthwise_conv(branch_features, branch_features, kernel_size=3, stride=self.stride, padding=1),
+            nn.BatchNorm2d(branch_features),
+            nn.Conv2d(branch_features, branch_features, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(branch_features),
+            nn.ReLU(inplace=True),
+        )
+
+    @staticmethod
+    def depthwise_conv(i, o, kernel_size, stride=1, padding=0, bias=False):
+        return nn.Conv2d(i, o, kernel_size, stride, padding, bias=bias, groups=i)
+
+    def forward(self, x):
+        if self.stride == 1:
+            x1, x2 = x.chunk(2, dim=1)
+            out = torch.cat((x1, self.branch2(x2)), dim=1)
+        else:
+            out = torch.cat((self.branch1(x), self.branch2(x)), dim=1)
+
+        out = channel_shuffle(out, 2)
+
+        return out
+
+class FeatureClassifier(nn.Module):
+    def __init__(self, layer_depth, bottleneck):
         super().__init__()
 
+        self.num_layers = layer_depth_dict[layer_depth]
+        self.bottleneck = bottleneck
         input_channels = 1
         output_channels = 50
-        bottleneck = 10
-        classes = 512
-        self.conv1 = nn.Sequential(
+        classes = layer_class_dict[layer_depth]
+        self.first_conv = nn.Sequential(
             nn.Conv2d(input_channels, output_channels, 3, 2, 1, bias=False),
             nn.BatchNorm2d(output_channels),
             nn.ReLU(inplace=True),
         )
-
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        shuffle_layers_dict = collections.OrderedDict()
+        for i in range(self.num_layers):
+            shuffle_layers_dict[str(i) + "res"] = InvertedResidual(output_channels, output_channels, 1)
+            shuffle_layers_dict[str(i) + "maxpool"] = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+            print(i)
+        
+        self.shuffleLayers = nn.Sequential(shuffle_layers_dict)
+        
+        self.final_conv = nn.Sequential(
+            nn.Conv2d(input_channels, output_channels, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(output_channels),
+            nn.ReLU(inplace=True),
+        )
+        self.fc_bottleneck = nn.Linear(output_channels, self.bottleneck)
+        self.fc_out = nn.Linear(self.bottleneck, classes)
 
-        self.fc_bottleneck = nn.Linear(output_channels, bottleneck)
-        self.fc_out = nn.Linear(bottleneck, classes)
 
     def forward(self, x):
-        x = self.conv1(x)
+        x = self.first_conv(x)
         x = self.maxpool(x)
+        if self.num_layers > 0:
+            x = self.shuffleLayers(x)
         x = x.mean([2,3])
         feat_vec = self.fc_bottleneck(x)
         class_probs = self.fc_out(feat_vec)
--- train_classifiers.py
+++ train_classifiers.py
@@ -1,42 +1,102 @@
+import os
 import torch
+import numpy as np
 from torch import nn, optim
 from torch.nn import functional as F
 from torch.autograd import Function
 from torchvision import datasets, transforms, utils
 from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+from tensorboardX import SummaryWriter
 
-from clustering_models import FeatureClassifier_L1
-
-transform = transforms.Compose([transforms.Grayscale(),transforms.ToTensor()])
-train_data = datasets.ImageFolder('/home/terence/data/network-bending/activations/1',  transform=transform)
-device = 'cuda'
+from clustering_models import FeatureClassifier
 
 
-data_loader = DataLoader(train_data, batch_size=100,shuffle=True)
-dataset = iter(data_loader)
+def train_classifier(layer, batch_size, n_epochs, bottleneck):
+    transform = transforms.Compose([transforms.Grayscale(),transforms.ToTensor()])
+    dataset = datasets.ImageFolder('/home/terence/data/network-bending/activations/'+str(layer),  transform=transform)
+    device = 'cuda'
+    writer = SummaryWriter()
+    validation_split = 0.1
+    dataset_len = len(dataset)
+    indices = list(range(dataset_len))
+    data_save_root = 'models/classifiers/'+str(layer)+"/"
+    if not os.path.exists(data_save_root):
+                os.makedirs(data_save_root)
 
-classifier_l1 = FeatureClassifier_L1().to(device)
-criterion = nn.CrossEntropyLoss()
-criterion.to(device)
-classifier_l1.train()
-optimizer = optim.Adam(classifier_l1.parameters(), lr=0.0001)
+    # Randomly splitting indices:
+    val_len = int(np.floor(validation_split * dataset_len))
+    validation_idx = np.random.choice(indices, size=val_len, replace=False)
+    train_idx = list(set(indices) - set(validation_idx))
 
-for step in range(100000):
-    try:
-        image, label = next(dataset)
-    except (OSError, StopIteration):
-        dataset = iter(data_loader)
-        image, label = next(dataset)
+    ## Defining the samplers for each phase based on the random indices:
+    train_sampler = SubsetRandomSampler(train_idx)
+    validation_sampler = SubsetRandomSampler(validation_idx)
 
-    image = image.to(device)
-    norm_image = ( image - 0.5 ) * 2
-    label = label.to(device)
-    vec, x_prob = classifier_l1(norm_image)
-    loss = criterion(x_prob, label)
-    loss = loss.to(device)
-    print("step: "+str(step).zfill(6) +", loss: " + str(loss))
-    loss.backward()
-    optimizer.step()
-    if step % 10000 == 0:
-        torch.save(classifier_l1.state_dict(), 'classifier1_'+str(step)+'.pt')
+    train_loader = torch.utils.data.DataLoader(dataset, sampler=train_sampler, batch_size=batch_size)
+    validation_loader = torch.utils.data.DataLoader(dataset, sampler=validation_sampler, batch_size=batch_size)
+    data_loaders = {"train": train_loader, "valid": validation_loader}
+    data_lengths = {"train": len(train_idx), "valid": val_len}
 
+    classifier = FeatureClassifier(layer,bottleneck).to(device)
+    criterion = nn.CrossEntropyLoss()
+    criterion.to(device)
+    lr = 0.0001
+    optimizer = optim.Adam(classifier.parameters(), lr=0.0001)
+
+    hparam_dict = {
+        "Layer" : layer,
+        "batch size" : batch_size,
+        "Learning rate": lr
+    }
+    writer.add_hparams(hparam_dict, {})
+    total_it = 0
+    for epoch in range(n_epochs):
+        print('Epoch {}/{}'.format(epoch, n_epochs - 1))
+        print('-' * 10)
+        classifier.zero_grad()
+        optimizer.zero_grad()
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'valid']:
+            if phase == 'train':
+                classifier.train(True)  # Set model to training mode
+            else:
+                classifier.train(False)  # Set model to evaluate mode
+
+            running_loss = 0.0
+            epoch_it = 0
+            for image, label in data_loaders[phase]:
+                classifier.zero_grad()
+                optimizer.zero_grad()
+                image = image.to(device)
+                norm_image = ( image - 0.5 ) * 2
+                label = label.to(device)
+                vec, x_prob = classifier(norm_image)
+                loss = criterion(x_prob, label)
+                loss = loss.to(device)
+                running_loss += loss.detach()
+                if phase == 'train':
+                    print("epoch: " + str(epoch) + ", step: "+str(epoch_it).zfill(6) +", training loss: " + str(float(loss)))
+                    writer.add_scalar('data/train_loss_continous', loss, total_it)
+                    loss.backward()
+                    optimizer.step()
+                    total_it +=1                # optimizer = scheduler(optimizer, epoch)
+                epoch_it +=1
+            
+            epoch_loss = running_loss / data_lengths[phase]
+            if phase == 'train':    
+                print("Epoch: "+str(epoch).zfill(6) +", train loss: " + str(epoch_loss))
+                writer.add_scalar('data/train_loss_epoch', epoch_loss, epoch)
+            if phase == 'valid':
+                print("Epoch: "+str(epoch).zfill(6) +", valid loss: " + str(epoch_loss))
+                writer.add_scalar('data/valid_loss_epoch', epoch_loss, epoch)
+
+        if epoch % 10 == 0:
+            torch.save(classifier.state_dict(), data_save_root+'/classifier'+str(layer)+'_'+str(epoch)+'.pt')    
+
+    torch.save(classifier.state_dict(), data_save_root +'/classifier'+str(layer)+'_final.pt')  
+    writer.export_scalars_to_json(data_save_root+"all_scalars.json")
+    writer.close()   
+
+
+train_classifier(1,10,11,10)
