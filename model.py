f8a0b3cbc22adf33f1fc0766a499c89bf732ceb2
Terence Broad <terry.m.broad@gmail.com>
2020-09-09 17:20:03 +0100

    Uncomment code for skip connections

--- model.py
+++ model.py
@@ -360,8 +360,7 @@ class ToRGB(nn.Module):
 
         if skip is not None:
             skip = self.upsample(skip)
-
-            # out = out + skip
+            out = out + skip
 
         return out
 
